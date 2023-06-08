f2ac6c3e88685b0d457e02176ec78798eca25c13
Evgeny_B <jonn-x008@yandex.ru>
2023-06-09 01:02:06 +0300

    Masa

---
+++ Example005_Condition/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string Username = Console.ReadLine();
+
+if(Username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же МАША !");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(Username);  
+}
